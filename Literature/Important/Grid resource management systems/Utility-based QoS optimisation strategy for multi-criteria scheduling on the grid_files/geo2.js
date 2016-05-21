caab07093693bcc6a76b59a063f2b4640fe8d9a9
Nishanth Nagendra <nishanth.amogh@gmail.com>
2016-05-22 00:09:23 +0200

    update

---
+++ Literature/Important/Grid resource management systems/Utility-based QoS optimisation strategy for multi-criteria scheduling on the grid_files/geo2.js
@@ -0,0 +1,16 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "MUNICH",
+      'continent': "EU",
+      'country': "DE",
+      'region': "BY"
+    },
+    'ip':"138.246.2.7"
+  }]);
+})
+//
+()
+
+;
